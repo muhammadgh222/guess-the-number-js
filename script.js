a28ee743d80b604e9ca6ea793a2d38de5d4e8422
devdavinci <dev.muhammad222000@gmail.com>
2022-07-02 19:05:46 +0200

    Solving the minus problem

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let secretNum = Math.trunc(Math.random() * 20) + 1;
-let score = 5;
+let score = 10;
 let highScore = 0;
 
 const showMessage = msg => {
@@ -13,15 +13,7 @@ document.querySelector('.check').addEventListener('click', () => {
   console.log(score, typeof score);
 
   if (!guess) {
-    if (score > 1) {
-      showMessage('No Number!');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      showMessage('You lost the game');
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('.number').textContent = secretNum;
-    }
+    showMessage('No Number!');
   } else if (guess === secretNum) {
     showMessage('Congratulations!');
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -35,10 +27,16 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.highscore').textContent = highScore;
     }
   } else if (guess !== secretNum) {
-    score--;
+    if (score > 1) {
+      score--;
 
-    showMessage(guess < secretNum ? 'Higher!' : 'Lower');
-    document.querySelector('.score').textContent = score;
+      showMessage(guess < secretNum ? 'Higher!' : 'Lower');
+      document.querySelector('.score').textContent = score;
+    } else {
+      showMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.number').textContent = secretNum;
+    }
   } else {
     showMessage('You lost the game');
     document.querySelector('.score').textContent = 0;
@@ -47,9 +45,9 @@ document.querySelector('.check').addEventListener('click', () => {
 });
 
 document.querySelector('.again').addEventListener('click', () => {
-  score = 5;
+  score = 10;
 
-  document.querySelector('.score').textContent = 5;
+  document.querySelector('.score').textContent = 10;
 
   secretNum = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('body').style.backgroundColor = '#222';
